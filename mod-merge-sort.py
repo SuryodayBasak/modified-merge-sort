b44a58ce705c5b24c82e4a6f30a0038301996586
Suryoday Basak <suryodaybasak@gmail.com>
2018-09-17 02:04:49 -0500

    finished coding the main procedure of modified merge sort

---
+++ mod-merge-sort.py
@@ -0,0 +1,58 @@
+def insertion_sort(A, p, r):
+    #print(A[p:r])
+    for j in range(p+1, r):
+        key = A[j]
+        #Insert A[j] into the sorted sequence A[0...j-1]
+        i = j-1
+
+        while ((i >= p) and (A[i] > key)):
+            #print(i+j)
+            A[i+1] = A[i]
+            i = i-1
+        A[i + 1] = key
+    #print(A[p:r])
+    #print()
+
+def merge(A, p, q, r):
+    n1 = q-p+1
+    n2 = r-q
+
+    L = []
+    R = []
+
+    for i in range(0, n1):
+        L.append(A[p+i])
+
+    for j in range(0, n2):
+        R.append(A[q+j+1])
+
+    L.append(99999)
+    R.append(99999)
+    
+    i = 0
+    j = 0
+
+    for k in range (p, r+1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i = i + 1
+        else:
+            A[k] = R[j]
+            j = j + 1
+
+def merge_sort(A, p, r, k):
+
+    if (r-p <= k):
+        insertion_sort(A, p, p+k)
+   
+    elif p < r:
+        q = int((p+r)/2)
+        merge_sort(A, p, q, k)
+        merge_sort(A, q+1, r, k)
+        merge(A, p, q, r)
+
+k = 4
+A = [99, 6462, 11, 7, 44, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 99, 6462, 11, 7, 44, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+print(A)
+merge_sort(A, 0, len(A)-1, k)
+print(A)
